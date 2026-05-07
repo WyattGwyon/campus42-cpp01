a52a5669bcb58b7b923a333d383e11dd2fc06c4d
Clayton Louden <claytonlouden@gmail.com>
2026-05-07 18:34:58 +0200

    missing files from last commit

---
+++ ex00/randomChump.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clouden <clouden@student.42madrid.com      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/06 19:53:27 by clouden           #+#    #+#             */
+/*   Updated: 2026/05/06 19:57:21 by clouden          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+using std::string;
+
+void	randomChump(string name)
+{
+	Zombie*	chump.newZombie("Chump");
+	chump.announce();
+	delete chump;
+}
+
